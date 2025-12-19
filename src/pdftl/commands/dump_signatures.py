78ffed697ab8b091618c10d56680456fc7ad3059
pdftl-dev <236203910+pdftl-dev@users.noreply.github.com>
2025-12-19 21:54:10 +0000

    tests: test coverage

--- src/pdftl/commands/dump_signatures.py
+++ src/pdftl/commands/dump_signatures.py
@@ -2,6 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+# src/pdftl/commands/dump_signatures.py
+
 """Dump and validate digital signatures from a PDF file using pyHanko"""
 
 import io
--- src/pdftl/output/sign.py
+++ src/pdftl/output/sign.py
@@ -1,4 +1,11 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 # src/pdftl/output/sign.py
+
+"""Signing PDF files using pyhanko"""
+
 import io
 import logging
 import os
